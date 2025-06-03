7406f76d5ad557a5c5d22f20700d7c8a76e98bb0
ZeRo-es <luisfalcon1908@gmail.com>
2025-06-03 10:24:02 +0200

    Update estres.sh

--- estres.sh
+++ estres.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 URL="https://wpluis1-cpajaxcwfheahkcp.spaincentral-01.azurewebsites.net"
-CONCURRENTES=50
+CONCURRENTES=100
 
 for i in $(seq 1 $CONCURRENTES); do
   while true; do
